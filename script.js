19fc80e5ac26f7053916377b99810f1c8fba2973
MAYANK PANDEY <101644584+mayank2021264@users.noreply.github.com>
2026-03-31 11:43:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	if((s1.length || s2.length) ===0) return -1;
+	let string1 = s1.toLowerCase();
+	let string2 = s2.toLowerCase();
+	return string1.indexOf(string2);
+	
 }
 
 // Please do not change the code below
+
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
